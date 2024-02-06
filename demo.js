9d8bcced6662d013fa3589277b52ecb3e7c861c8
lopanath <lopanath0032gmail.com>
2024-02-06 12:47:45 +0000

    1st

---
+++ demo.js
@@ -0,0 +1,27 @@
+console.log("Hii---");
+const desc=document.querySelector("#b1");
+const msg=document.querySelector("#p1");
+desc.addEventListener("click",myFun);
+function myFun()
+{
+   
+    
+    if (msg.style.visibility === "hidden") {
+    msg.style.visibility = "visible";
+    }
+    else
+    msg.style.visibility = "hidden";
+}
+
+
+
+desc.addEventListener("mouseover",changeColor);
+function changeColor()
+{
+    desc.style.backgroundColor="red";
+}
+desc.addEventListener("mouseout",retainColor)
+function retainColor()
+{
+    desc.style.backgroundColor="blueviolet";
+}
